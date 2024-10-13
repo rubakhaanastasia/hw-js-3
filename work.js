b577d4b62d25cdeec989259e3e36a9d44b7b6e1f
rubakhaanastasia <145857108+rubakhaanastasia@users.noreply.github.com>
2024-10-13 22:52:59 +0200

    Add files via upload

---
+++ work.js
@@ -0,0 +1,21 @@
+const result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof result);
+
+const email = "jemandestraum@gmail.com";
+const mySymbol = email.includes("@");
+const symbol = email.length;
+console.log(mySymbol);
+console.log(symbol);
+
+let expression1 = "my";
+let expression2 = "name";
+let expression3 = "is";
+let fullName = `${expression1} ${expression2} ${expression3}`;
+fullName += ' Viktor';
+console.log(fullName);
+
+const userName = "Олександро";
+const payment = 300;
+const message = "Дякуємо, Олександро! До сплати 300 гривень";
+alert(message);
